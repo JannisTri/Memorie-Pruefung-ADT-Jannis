17c841f0470d6d413a9edac6150725f1d8d8f46b
JannisTri <Jannis13x13@gmail.com>
2023-02-07 18:43:44 +0100

    fixed function assignCards and merged it into function shuffleCards

--- script.js
+++ script.js
@@ -1,31 +1,18 @@
-const cards =["/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_1.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_1.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_2.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_2.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_3.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_3.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_4.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_4.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_5.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_5.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_6.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_6.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_7.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_7.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_8.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_8.png",];
-const newCards = [...cards]; 
 let x; 
 let y= 0;
 function shuffleCards(){                                           //Sorgt dafür das die zufallszahl nur genau einmal vorkommt indem es das Array Cards in ein neues array kopiert, ausdem später mithilfe des splice befehls die schon verwendeten werte gelöscht werden ohne das ursprüngliche array zu löschen    
+    const cards =["/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_1.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_1.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_2.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_2.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_3.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_3.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_4.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_4.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_5.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_5.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_6.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_6.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_7.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_7.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_8.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_8.png",];
+    const newCards = [...cards];       
     for (i=0; i<16; i++ ){
-         x = Math.floor((Math.random() * newCards.length));
-        // console.log(newCards[x]);
-        
+        x = Math.floor((Math.random() * newCards.length));
         y++;
         let motive = document.getElementById("F" + y);
         motive.src = newCards[x];
-        console.log(newCards[x]);
-        console.log (motive);
-        // funktuniert noch nicht Warum?
         newCards.splice(x,1);
     }
-    
 }
-// function assignCards(){
-//         y++;
-//         console.log(y);
-//         let motive = document.getElementById("F" + y);
-//         motive.src = newCards[x];
-//         console.log (motive)
 
+
+// function checkMatch(){
+    
 // }
-
-function checkMatch(){
-    
-}
