0ba2b7fcd5adff58d4629ac264d7223a3050cc28
JannisTri <Jannis13x13@gmail.com>
2023-02-07 23:08:20 +0100

    added function resetCards

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let x; 
+const cards =["/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_1.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_1.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_2.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_2.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_3.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_3.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_4.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_4.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_5.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_5.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_6.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_6.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_7.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_7.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_8.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_8.png",];
+var x; 
 let y= 0;
-function shuffleCards(){                                           //Sorgt dafür das die zufallszahl nur genau einmal vorkommt indem es das Array Cards in ein neues array kopiert, ausdem später mithilfe des splice befehls die schon verwendeten werte gelöscht werden ohne das ursprüngliche array zu löschen    
-    const cards =["/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_1.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_1.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_2.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_2.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_3.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_3.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_4.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_4.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_5.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_5.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_6.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_6.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_7.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_7.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_8.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_8.png",];
-    const newCards = [...cards];       
+function shuffleCards(){                                  //Sorgt dafür das die zufallszahl nur genau einmal vorkommt indem es das Array Cards in ein neues array kopiert, ausdem später mithilfe des splice befehls die schon verwendeten werte gelöscht werden ohne das ursprüngliche array zu löschen    
+    let newCards = [...cards];       
     for (i=0; i<16; i++ ){
         x = Math.floor((Math.random() * newCards.length));
         y++;
@@ -10,9 +10,19 @@ function shuffleCards(){                                           //Sorgt dafü
         motive.src = newCards[x];
         newCards.splice(x,1);
     }
+  
 }
-
 
 // function checkMatch(){
     
 // }
+
+function resetCards(){
+    y = 0
+    for (i=0; i<16; i++ ){
+    y++;
+    let motive = document.getElementById("F" + y);
+    motive.src = "/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_Cover.png"
+}
+    y=0
+}
