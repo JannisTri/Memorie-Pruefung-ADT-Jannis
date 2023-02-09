ca08b0f0bb0847be11b8bc929324dbe55c51ea2b
JannisTri <Jannis13x13@gmail.com>
2023-02-09 22:40:24 +0100

    Edited shuffleCards function, to work with the new function flipCard

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 const cards =["/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_1.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_1.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_2.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_2.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_3.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_3.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_4.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_4.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_5.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_5.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_6.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_6.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_7.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_7.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_8.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_8.png",];
-var x; 
-let y= 0;
+const container = document.getElementById("Spielfeld");
+let x; 
+let motive;
+let y=0;
+let index;
+let CardMotivs = [];
+shuffleCards();
 function shuffleCards(){                                  //Sorgt dafür das die zufallszahl nur genau einmal vorkommt indem es das Array Cards in ein neues array kopiert, ausdem später mithilfe des splice befehls die schon verwendeten werte gelöscht werden ohne das ursprüngliche array zu löschen    
     let newCards = [...cards];       
     for (i=0; i<16; i++ ){
         x = Math.floor((Math.random() * newCards.length));
-        y++;
-        let motive = document.getElementById("F" + y);
-        motive.src = newCards[x];
+        CardMotivs.push(newCards[x]);
         newCards.splice(x,1);
     }
-  
 }
 
-// function checkMatch(){
-    
-// }
+container.addEventListener("click", flipCard);
+
+function flipCard(event){
+   index = event.target.id;
+   let Card = document.getElementById(index);
+   Card.src = CardMotivs[index - 1];
+}
 
 function resetCards(){
-    y = 0
     for (i=0; i<16; i++ ){
     y++;
-    let motive = document.getElementById("F" + y);
+    motive = document.getElementById(y);
     motive.src = "/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_Cover.png"
 }
     y=0
+    shuffleCards();
 }
+// ToDo für morgen reset Cards und shuffel cards in eine Funktion schreiben
