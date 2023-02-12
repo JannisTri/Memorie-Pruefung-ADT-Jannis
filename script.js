5697d41fc0b1a2f36128cbc81b3f81ec40492027
JannisTri <Jannis13x13@gmail.com>
2023-02-12 14:46:28 +0100

    Added Victory Text & adjustet Reset Button

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const cards =["/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_1.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_1.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_2.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_2.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_3.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_3.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_4.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_4.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_5.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_5.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_6.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_6.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_7.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_7.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_8.png","/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_8.png",];
 const container = document.getElementById("Spielfeld");
 let Highscore = document.getElementById("Highscore");
-let MatchCounter = 0;
-let BetrX = 1;
+let WinNot = document.getElementById("winNotfication");
+let MatchCounter;
+let BetrX = 2;
 let x; 
 let Card1;
 let Card2;
@@ -15,6 +16,9 @@ function resetCards(){
     let newCards = [...cards]; 
     let y= 0;
     CardMotivs = [];
+    Highscore.innerText = "0";
+    MatchCounter = 0;
+    WinNot.innerText = " ";
     for (i=0; i<16; i++ ){
         x = Math.floor((Math.random() * newCards.length));
         CardMotivs.push(newCards[x]);
@@ -23,7 +27,7 @@ function resetCards(){
     for (i=0; i<16; i++ ){
         y++;
         let motive = document.getElementById(y);
-        motive.src = "/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_Cover.png"
+        motive.src = "/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_Cover.png";
     }        
 }
 // eventListener und Funktion sorgen dafür,dass die richtige Vorderseite der Karte angezeigt wird, wenn man auf das Cover Motiv drückt und ruft checkmatch auaf wenn die 2. Karte angeklickt wurde
@@ -34,24 +38,22 @@ function flipCard(event){
    let Card = document.getElementById(index);
    Card.src = CardMotivs[index - 1];
    if(counter == 1){
-        Card1 = Card 
+        Card1 = Card; 
    } else {
-        Card2 = Card
-        counter = 0
-        setTimeout (checkMatch, 3000)
+        Card2 = Card;
+        counter = 0;
+        setTimeout (checkMatch, 3000);
    }
-   console.log(Card1,Card2)
-
 }
 // überprüft die Werte und versteckt die karten wieder wenn sie nicht gleich sind
 function checkMatch(){
     if (Card1.src===Card2.src) {
-        console.log("Yaay")
+        console.log("Yaay");
         setHighscoreUp();
     } else if (Card2 !== undefined ){
-        console.log("Noooo")
-        Card1.src="/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_Cover.png"
-        Card2.src="/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_Cover.png"
+        console.log("Noooo");
+        Card1.src="/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_Cover.png";
+        Card2.src="/GitHub/Memorie-Pruefung-ADT-Jannis/SpaceCat_Cover.png";
         setHighscoreDown();
     }
 }
@@ -64,12 +66,11 @@ function setHighscoreDown(){
     if (MatchCounter > 0){
     Highscore.innerText = parseInt(Highscore.innerText) - (BetrX*0.5);}
     if (Highscore.innerText < 0){
-        Highscore.innerText = "0"    }
-
+        Highscore.innerText = "0";}
 }
 function checkWin(){
     if(MatchCounter == 8){
-
-        // innerText = "Herzlichen Glückwunsch, du hast das Spiel erfolgreich beendet. Dein Highscore ist:" + Highscore.innerText
+        console.log("Win")
+        WinNot.innerText = "Herzlichen Glückwunsch, du hast das Spiel erfolgreich beendet. Dein Highscore ist: " + Highscore.innerText
     }
 }
